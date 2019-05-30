3990925486ab420d3d11bed21a543965db8f186a
bfks <sedamdana7321>
2019-05-30 15:28:15 +0200

    Initial commit

---
+++ Test/DatumTest.java
@@ -0,0 +1,202 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class DatumTest {
+
+    @Test
+    void TestKonstruktora() {
+        assertThrows(Exception.class, () -> {
+            Datum d;
+            d = new Datum(-1,1,1);
+            d = new Datum(29,2,2000);
+            d = new Datum(31,6,2000);
+        });
+    }
+    @Test
+    void TestpomjeriZaJedanDanUnaprijed() {
+        Datum d;
+        boolean ok = false;
+        try{
+            d = new Datum(22,6,2000);
+            d.pomjeriZaJedanDanUnaprijed();
+            d.compareTo(new Datum(23,6,2000));
+            ok = true;
+        }catch(Exception e){
+            ok = false;
+        }
+
+        assertEquals(true, ok);
+    }
+    @Test
+    void TestpomjeriZaJedanDanUnaprijed1() {
+        Datum d;
+        boolean ok = false;
+        try{
+            d = new Datum(30,6,2000);
+            d.pomjeriZaJedanDanUnaprijed();
+            d.compareTo(new Datum(1,7,2000));
+            ok = true;
+        }catch(Exception e){
+            ok = false;
+        }
+
+        assertEquals(true, ok);
+    }
+    @Test
+    void TestpomjeriZaJedanDanUnaprijed2() {
+        Datum d;
+        boolean ok = false;
+        try{
+            d = new Datum(31,12,2000);
+            d.pomjeriZaJedanDanUnaprijed();
+            d.compareTo(new Datum(1,1,2001));
+            ok = true;
+        }catch(Exception e){
+            ok = false;
+        }
+
+        assertEquals(true, ok);
+    }
+    @Test
+    void TestpomjeriZaJedanDanUnaprijed3() {
+        Datum d;
+        boolean ok = false;
+        try{
+            d = new Datum(28,2,2000);
+            d.pomjeriZaJedanDanUnaprijed();
+            d.compareTo(new Datum(29,2,2001));
+            ok = true;
+        }catch(Exception e){
+            ok = false;
+        }
+
+        assertEquals(true, ok);
+    }
+    @Test
+    void TestPomjeriZaJedanDanUnazad() {
+        Datum d;
+        boolean ok = false;
+        try{
+            d = new Datum(28,2,2000);
+            d.pomjeriZaJedanDanUnazad();
+            d.compareTo(new Datum(27,2,2000));
+            ok = true;
+        }catch(Exception e){
+            ok = false;
+        }
+
+        assertEquals(true, ok);
+    }
+    @Test
+    void TestPomjeriZaJedanDanUnazad1() {
+        Datum d;
+        boolean ok = false;
+        try{
+            d = new Datum(1,3,2000);
+            d.pomjeriZaJedanDanUnazad();
+            d.compareTo(new Datum(29,2,2000));
+            ok = true;
+        }catch(Exception e){
+            ok = false;
+        }
+
+        assertEquals(true, ok);
+    }
+    @Test
+    void TestPomjeriZaJedanDanUnazad2() {
+        Datum d;
+        boolean ok = false;
+        try{
+            d = new Datum(1,3,2001);
+            d.pomjeriZaJedanDanUnazad();
+            d.compareTo(new Datum(28,2,2001));
+            ok = true;
+        }catch(Exception e){
+            ok = false;
+        }
+
+        assertEquals(true, ok);
+    }
+
+    @Test
+    void TestToString() {
+        Datum d = null;
+        boolean ok = false;
+        try{
+            d = new Datum(1,3,2001);
+            ok = true;
+        }catch(Exception e){
+            ok = false;
+        }
+        assertEquals(true, d.toString().equals("1/3/2001") && ok);
+    }
+    @Test
+    void TestEquals() {
+        Datum d = null;
+        Datum d1 = null;
+        boolean ok = false;
+        try{
+            d = new Datum(1,3,2001);
+            d1 = new Datum(1,3,2001);
+            ok = true;
+        }catch(Exception e){
+            ok = false;
+        }
+        assertEquals(true, d.equals(d1) && ok);
+    }
+    @Test
+    void TestEquals1() {
+        Datum d = null;
+        Datum d1 = null;
+        boolean ok = false;
+        try{
+            d = new Datum(1,3,2001);
+            d1 = new Datum(2,3,2001);
+            ok = true;
+        }catch(Exception e){
+            ok = false;
+        }
+        assertEquals(true, (!d.equals(d1)) && ok);
+    }
+    @Test
+    void TestCompareTo() {
+        Datum d = null;
+        Datum d1 = null;
+        int ok = 5;
+        try{
+            d = new Datum(1,3,2001);
+            d1 = new Datum(2,3,2001);
+            ok = 0;
+        }catch(Exception e){
+        }
+        assertEquals(1, d1.compareTo(d) + ok);
+    }
+    @Test
+    void TestCompareTo1() {
+        Datum d = null;
+        Datum d1 = null;
+        int ok = 5;
+        try{
+            d = new Datum(1,3,2001);
+            d1 = new Datum(2,3,2001);
+            ok = 0;
+        }catch(Exception e){
+        }
+        assertEquals(-1, d.compareTo(d1) + ok);
+    }
+    @Test
+    void TestCompareTo2() {
+        Datum d = null;
+        Datum d1 = null;
+        int ok = 5;
+        try{
+            d = new Datum(1,3,2001);
+            d1 = new Datum(2,3,2001);
+            ok = 0;
+        }catch(Exception e){
+        }
+        assertEquals(0, d.compareTo(d) + ok);
+    }
+}
---
+++ src/Datum.java
@@ -0,0 +1,51 @@
+public class Datum implements Comparable<Datum>{
+    private int dan, mjesec, godina;
+    private boolean jesuLiDatumiJednaki(Datum d1, Datum d2){
+        //kod
+        return true; //ovo izbrisati pa svoje ubaciti
+    }
+
+    private Datum(){
+        //ovdje nista ne upisivati,
+        //konstruktor bez parametara je oznacen kao private
+        // i služi da zabrani kreiranje "prazog" datuma naredbom :
+        // Datum d = new Datum();
+        // Za kreiranje objekata ce se samo koristiti konstruktor sa 3 parametra
+        // koji se nalazi ispod!
+    }
+
+    public Datum(int dan, int mjesec, int godina) throws Exception{
+        //kod
+    }
+
+
+    public void pomjeriZaJedanDanUnaprijed(){
+        //kod
+    }
+    public void pomjeriZaJedanDanUnazad(){
+        //kod
+    }
+
+
+    @Override
+    public String toString(){
+        //kod
+        return ""; //ovo izbrisati pa svoje ubaciti
+    }
+
+    @Override
+    public boolean equals(Object o){
+        //ovu funkciju ne prepravljati!
+        if(o == null) return false;
+        else{
+            Datum d = (Datum) o;
+            return jesuLiDatumiJednaki(this, d);
+        }
+    }
+
+    @Override
+    public int compareTo(Datum d){
+        //kod
+        return 0; //ovo izbrisati pa svoje ubaciti
+    }
+}
